a32f19592849d8a10d0dc97b5e95883736811e2b
ludik <bakhanetssimon@gmail.com>
2026-01-04 15:35:30 +0700

    Initial Commit

---
+++ Momentum Drives Success/Momentum_Drives_SuccessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Momentum_Drives_SuccessApp.swift
+//  Momentum Drives Success
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Momentum_Drives_SuccessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Momentum Drives SuccessTests/Momentum_Drives_SuccessTests.swift
@@ -0,0 +1,16 @@
+//
+//  Momentum_Drives_SuccessTests.swift
+//  Momentum Drives SuccessTests
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import Testing
+
+struct Momentum_Drives_SuccessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
